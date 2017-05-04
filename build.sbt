b8fe260423034ca7c4871034ff8945814a5e3bbe
vassilis simeonidis <vassilis.simeonidis@camelotglobal.com>
2017-05-04 15:23:12 +0300

    added mllib and als algorithm with MSE

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ name := "RecomenderComparison"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.10.4"
 
-libraryDependencies ++= Seq("org.apache.spark" % "spark-core_2.11" % "1.6.0")
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "1.3.1" % "provided",
+  "org.apache.spark" %% "spark-mllib" % "1.3.1"
+)
--- src/main/scala/MainObject.scala
+++ src/main/scala/MainObject.scala
@@ -1,7 +1,8 @@
 import org.apache.spark.{SparkConf, SparkContext}
-/**
-  * Created by vasilis on 11/04/17.
-  */
+import org.apache.spark.mllib.recommendation.ALS
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+import org.apache.spark.mllib.recommendation.Rating
+
 object MainObject {
 
   val conf = new SparkConf()
@@ -14,20 +15,50 @@ object MainObject {
     val genres = sc.textFile("ml-100k/u.genre").map(u => u.trim.split("\\|")).cache()
     val items = sc.textFile("ml-100k/u.item").map(u => u.trim.replace("||", "|").split("\\|")).cache()
     val occupations = sc.textFile("ml-100k/u.occupation").cache()
-    val rattings = sc.textFile("ml-100k/u.data").map(u => u.trim.split(" ")).cache()
+    val ratings = sc.textFile("ml-100k/u.data").map(_.split("\t")  match { case Array(user, item, rate, timestamp) =>
+      Rating(user.toInt, item.toInt, rate.toDouble)
+    }).cache()
 
-    val femaleUsers = users.filter(u => u(2) == "F").count()
 
-    println("----------------------------")
-    println(users.count())
-    println(genres.count())
-    println(items.count())
-    println(occupations.count())
-    println(rattings.count())
-    println("----------------------------")
-    println(femaleUsers)
-    println(items.first()(3))
-    println("----------------------------")
+
+    // Build the recommendation model using ALS
+    val rank = 10
+    val numIterations = 10
+    val model = ALS.train(ratings, rank, numIterations, 0.01)
+
+    // Evaluate the model on rating data
+    val usersProducts = ratings.map { case Rating(user, product, rate) =>
+      (user, product)
+    }
+    val predictions =
+      model.predict(usersProducts).map { case Rating(user, product, rate) =>
+        ((user, product), rate)
+      }
+    val ratesAndPreds = ratings.map { case Rating(user, product, rate) =>
+      ((user, product), rate)
+    }.join(predictions)
+    val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
+      val err = (r1 - r2)
+      err * err
+    }.mean()
+    println("Mean Squared Error = " + MSE)
+
+    // Save and load model
+    model.save(sc, "target/tmp/myCollaborativeFilter")
+    val sameModel = MatrixFactorizationModel.load(sc, "target/tmp/myCollaborativeFilter")
+
+//    val femaleUsers = users.filter(u => u(2) == "F").count()
+//
+//    println("----------------------------")
+//    println(users.count())
+//    println(genres.count())
+//    println(items.count())
+//    println(occupations.count())
+//    println(ratings.count())
+//    println("----------------------------")
+//    println(femaleUsers)
+//    println(ratings.first()(0) + " " + ratings.first()(1) + " " +ratings.first()(2) + " " + ratings.first()(3))
+//    println("----------------------------")
 
   }
 }
