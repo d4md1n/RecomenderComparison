8689e801328a93e3be63899fd190856f7e10ab22
vassilis simeonidis <vassilis.simeonidis@camelotglobal.com>
2017-05-20 15:45:32 +0300

    added MSE as measurment

--- src/main/scala/MainObject.scala
+++ src/main/scala/MainObject.scala
@@ -4,79 +4,61 @@ import org.apache.spark.mllib.recommendation.Rating
 
 object MainObject {
 
-  val conf = new SparkConf()
+  val sparkConfiguration = new SparkConf()
     .setMaster("local[4]")
     .setAppName("RecomenderComparison")
-  val sc = new SparkContext(conf)
+  val sparkContext = {
+    val sc = new SparkContext(sparkConfiguration)
+    sc.setCheckpointDir("checkpoint/") // set checkpoint dir to avoid stack overflow
+    sc
+  }
 
 
   def main(args: Array[String]) {
 
-    sc.setCheckpointDir("checkpoint/") // set checkpoint dir to avoid stack overflow
-
-
     //import data to rdd
-    val users = sc.textFile("ml-100k/u.user").map(u => u.trim.split("\\|")).cache()
-    val genres = sc.textFile("ml-100k/u.genre").map(u => u.trim.split("\\|")).cache()
-    val items = sc.textFile("ml-100k/u.item").map(u => u.trim.replace("||", "|").split("\\|")).cache()
-    val occupations = sc.textFile("ml-100k/u.occupation").cache()
-    val ratings = sc.textFile("ml-100k/u1.base").map(_.split("\t")  match { case Array(user, item, rate, timestamp) =>
+    val users = sparkContext.textFile("ml-100k/u.user").map(u => u.trim.split("\\|")).cache()
+    val genres = sparkContext.textFile("ml-100k/u.genre").map(u => u.trim.split("\\|")).cache()
+    val items = sparkContext.textFile("ml-100k/u.item").map(u => u.trim.replace("||", "|").split("\\|")).cache()
+    val occupations = sparkContext.textFile("ml-100k/u.occupation").cache()
+    val ratings = sparkContext.textFile("ml-100k/u1.base").map(_.split("\t")  match { case Array(user, item, rate, timestamp) =>
+    Rating(user.toInt, item.toInt, rate.toDouble)
+    }).cache()
+
+    //// Build the recommendation model using ALS
+    val rank = 20          // 10 - 20
+    val numIterations = 80 // 50 - 100
+    val model = ALS.train(ratings, rank, numIterations, 0.01) // pollaplasia 3
+
+
+    //// compare algorithms on prediction, user recommendation, product recommendation
+    //import test dataset
+    val testRatings = sparkContext.textFile("ml-100k/u1.test").map(_.split("\t") match { case Array(user, item, rate, timestamp) =>
       Rating(user.toInt, item.toInt, rate.toDouble)
     }).cache()
 
+    // remove rating from dataset
+    val usersProducts = testRatings.map {
+      case Rating(user, product, rate) => (user, product)
+    }
+
+    // predict the rating
+    val predictions = model.predict(usersProducts).map {
+      case Rating(user, product, rate) => ((user, product), rate)
+    }
+
+    // join rdd to get the rating and the prediction value for each combination
+    val ratesAndPreds = testRatings.map {
+      case Rating(user, product, rate) => ((user, product), rate)
+    }.join(predictions)
+
+    // calculate MSE for the whole rdd
+    val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
+      val err = (r1 - r2)
+      err * err
+    }.mean()
 
 
-    // Build the recommendation model using ALS
-    val rank = 10          // 10 - 20
-    val numIterations = 80 // 50 - 100
-
-
-    val model = ALS.train(ratings, rank, numIterations, 0.01) // pollaplasia 3
-
-    //load test model
-// compare algorithms on prediction, user recommendation, product recommendation
-    val testRatings = sc.textFile("ml-100k/u1.test").map(_.split("\t")).cache()
-
-    val testSubject = testRatings.first()
-    val prediction = model.predict(testSubject(0).toInt, testSubject(1).toInt)
-    ////
-
-    println("prediction : " + prediction + "real number : " + testSubject(2))
-//
-//    //evaluation///
-//    // Evaluate the model on rating data
-//    val usersProducts = ratings.map { case Rating(user, product, rate) =>
-//      (user, product)
-//    }
-//    val predictions =
-//      model.predict(usersProducts).map { case Rating(user, product, rate) =>
-//        ((user, product), rate)
-//      }
-//    val ratesAndPreds = ratings.map { case Rating(user, product, rate) =>
-//      ((user, product), rate)
-//    }.join(predictions)
-//    val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
-//      val err = (r1 - r2)
-//      err * err
-//    }.mean()
-//    println("Mean Squared Error = " + MSE)
-//
-//    // Save and load model
-//    model.save(sc, "target/tmp/myCollaborativeFilter")
-//    val sameModel = MatrixFactorizationModel.load(sc, "target/tmp/myCollaborativeFilter")
-
-//    val femaleUsers = users.filter(u => u(2) == "F").count()
-//
-//    println("----------------------------")
-//    println(users.count())
-//    println(genres.count())
-//    println(items.count())
-//    println(occupations.count())
-//    println(ratings.count())
-//    println("----------------------------")
-//    println(femaleUsers)
-//    println(ratings.first()(0) + " " + ratings.first()(1) + " " +ratings.first()(2) + " " + ratings.first()(3))
-//    println("----------------------------")
-
+    println("Mean Squared Error = " + MSE)
   }
 }
